39164831abc49379596ccc5a828277284a70bee3
Igor Sidorov <codeBurger770@gmail.com>
2020-07-16 15:47:43 +0300

    Исправлен баг в authRouter

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -10,6 +10,6 @@ authRouter.post('/auth/signin', authController.signin)
 authRouter.post('/auth/signout', authController.signout)
 authRouter.post('/auth/refresh-tokens', authController.refreshTokens)
 authRouter.post('/auth/change-password-1', authController.changePassword1)
-authRouter.post('/auth/change-password-1', authController.changePassword2)
+authRouter.post('/auth/change-password-2', authController.changePassword2)
 
 module.exports = authRouter
